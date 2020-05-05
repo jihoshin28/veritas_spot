b1658a394d62fa9d01c1ec1506a7dc1c6df07905
Allen Shin <geeuho@gmail.com>
2020-05-04 20:42:43 -0700

    hello

--- src/App.js
+++ src/App.js
@@ -10,14 +10,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <p>
+            Hello
+          </p>
+        </div>
       </header>
     </div>
   );
